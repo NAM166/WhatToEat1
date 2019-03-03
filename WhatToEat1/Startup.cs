537f9ebe68fd4b3bc4f8b961e5b346b0c4dd72c1
Niall <niallmcclelland166@hotmail.com>
2019-03-03 19:34:48 +0000

    Add project files.

---
+++ WhatToEat1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhatToEat1.Startup))]
+namespace WhatToEat1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
